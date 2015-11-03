047ac07ce480785c2b3e9bfeb3598ce71a0ad53d
info@gdinformatica.it <info@gdinformatica.it>
2015-11-03 17:14:33 +0100

    C

--- app.js
+++ app.js
@@ -1,168 +1,242 @@
-// JavaScript code for the BLE Scan example app.
+//
+// Copyright 2014, Andreas Lundquist
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// DFRobot - Bluno - Hello World
+// version: 0.1 - 2014-11-21
+//
 
-// Application object.
+// Route all console logs to Evothings studio log
+if (window.hyper && window.hyper.log) { console.log = hyper.log; };
+
+document.addEventListener(
+	'deviceready',
+	function() { evothings.scriptsLoaded(app.initialize) },
+	false);
+
 var app = {};
 
-// Device list.
-app.devices = {};
-
-// UI methods.
-app.ui = {};
-
-// Timer that updates the device list and removes inactive
-// devices in case no devices are found by scan.
-app.ui.updateTimer = null;
+//app.DFRBLU_SERVICE_UUID = 'dfb0';
+app.DFRBLU_SERVICE_UUID = '0000dfb0-0000-1000-8000-00805f9b34fb';
+app.DFRBLU_CHAR_RXTX_UUID = '0000dfb1-0000-1000-8000-00805f9b34fb';
+app.DFRBLU_TX_UUID_DESCRIPTOR = '00002902-0000-1000-8000-00805f9b34fb';
 
 app.initialize = function()
 {
-	document.addEventListener(
-		'deviceready',
-		function() { evothings.scriptsLoaded(app.onDeviceReady) },
-		false);
+	app.connected = false;
 };
 
-app.onDeviceReady = function()
+app.startScan = function()
 {
+	app.disconnect();
 
-    if (!window['postMessage'])
-        alert("oh crap");
-    else {
-        if (window.addEventListener) {
-            window.addEventListener("message", ReceiveMessage, false);
-        }
-        else {
-            window.attachEvent("onmessage", ReceiveMessage);
-        }
-    }
-};
+	console.log('Scanning started...');
 
-
-// Start the scan. Call the callback function when a device is found.
-// Format:
-//   callbackFun(deviceInfo, errorCode)
-//   deviceInfo: address, rssi, name
-//   errorCode: String
-app.startScan = function(callbackFun)
-{
-	app.stopScan();
-
-	evothings.ble.startScan(
-		function(device)
-		{
-			// Report success. Sometimes an RSSI of +127 is reported.
-			// We filter out these values here.
-			if (device.rssi <= 0)
-			{
-				callbackFun(device, null);
-			}
-		},
-		function(errorCode)
-		{
-			// Report error.
-			callbackFun(null, errorCode);
-		}
-	);
-};
-
-// Stop scanning for devices.
-app.stopScan = function()
-{
-	evothings.ble.stopScan();
-};
-
-// Called when Start Scan button is selected.
-app.ui.onStartScanButton = function()
-{
-	app.startScan(app.ui.deviceFound);
-	app.ui.displayStatus('Scanning...');
-	app.ui.updateTimer = setInterval(app.ui.displayDeviceList, 500);
-};
-
-// Called when Stop Scan button is selected.
-app.ui.onStopScanButton = function()
-{
-	app.stopScan();
 	app.devices = {};
-	app.ui.displayStatus('Scan Paused');
-	app.ui.displayDeviceList();
-	clearInterval(app.ui.updateTimer);
-};
 
-// Called when a device is found.
-app.ui.deviceFound = function(device, errorCode)
-{
-	if (device)
+	var htmlString =
+		'<img src="img/loader_small.gif" ' +
+			'style="display:inline; vertical-align:middle">' +
+		'<p style="display:inline">   Scanning...</p>';
+
+	$('#scanResultView').append($(htmlString));
+
+	$('#scanResultView').show();
+
+	function onScanSuccess(device)
 	{
-		// Set timestamp for device (this is used to remove
-		// inactive devices).
-		device.timeStamp = Date.now();
-
-		// Insert the device into table of found devices.
-		app.devices[device.address] = device;
-	}
-	else if (errorCode)
-	{
-		app.ui.displayStatus('Scan Error: ' + errorCode);
-	}
-};
-
-// Display the device list.
-app.ui.displayDeviceList = function()
-{
-	// Clear device list.
-	$('#found-devices').empty();
-
-	var timeNow = Date.now();
-
-	$.each(app.devices, function(key, device)
-	{
-		// Only show devices that are updated during the last 10 seconds.
-		if (device.timeStamp + 10000 > timeNow)
+		if (device.name != null)
 		{
-			// Map the RSSI value to a width in percent for the indicator.
-			var rssiWidth = 100; // Used when RSSI is zero or greater.
-			if (device.rssi < -100) { rssiWidth = 0; }
-			else if (device.rssi < 0) { rssiWidth = 100 + device.rssi; }
+			app.devices[device.address] = device;
 
-			// Create tag for device data.
-			var element = $(
-				'<li>'
-				+	'<strong>' + device.name + '</strong><br />'
-				// Do not show address on iOS since it can be confused
-				// with an iBeacon UUID.
-				+	(evothings.os.isIOS() ? '' : device.address + '<br />')
-				+	device.rssi + '<br />'
-				+ 	'<div style="background:rgb(225,0,0);height:20px;width:'
-				+ 		rssiWidth + '%;"></div>'
-				+ '</li>'
-			);
+			console.log(
+				'Found: ' + device.name + ', ' +
+				device.address + ', ' + device.rssi);
 
-			$('#found-devices').append(element);
+			var htmlString =
+				'<div class="deviceContainer" onclick="app.connectTo(\'' +
+					device.address + '\')">' +
+				'<p class="deviceName">' + device.name + '</p>' +
+				'<p class="deviceAddress">' + device.address + '</p>' +
+				'</div>';
+
+			$('#scanResultView').append($(htmlString));
 		}
-	});
+	}
+
+	function onScanFailure(errorCode)
+	{
+		// Show an error message to the user
+		app.disconnect('Failed to scan for devices.');
+
+		// Write debug information to console.
+		console.log('Error ' + errorCode);
+	}
+
+	evothings.easyble.reportDeviceOnce(true);
+	evothings.easyble.startScan(onScanSuccess, onScanFailure);
+
+	$('#startView').hide();
 };
 
-// Display a status message
-app.ui.displayStatus = function(message)
+app.setLoadingLabel = function(message)
 {
-	$('#scan-status').html(message);
+	console.log(message);
+	$('#loadingStatus').text(message);
+}
+
+app.connectTo = function(address)
+{
+	device = app.devices[address];
+
+	$('#loadingView').css('display', 'table');
+
+	app.setLoadingLabel('Trying to connect to ' + device.name);
+
+	function onConnectSuccess(device)
+	{
+		function onServiceSuccess(device)
+		{
+			// Application is now connected
+			app.connected = true;
+			app.device = device;
+
+			console.log('Connected to ' + device.name);
+
+			$('#loadingView').hide();
+			$('#scanResultView').hide();
+			$('#controlView').show();
+
+			device.enableNotification(
+				app.DFRBLU_CHAR_RXTX_UUID,
+				app.receivedData,
+				function(errorcode) {
+					console.log('BLE enableNotification error: ' + errorCode);
+				});
+		}
+
+		function onServiceFailure(errorCode)
+		{
+			// Disconnect and show an error message to the user.
+			app.disconnect('Device is not from DFRobot');
+
+			// Write debug information to console.
+			console.log('Error reading services: ' + errorCode);
+		}
+
+		app.setLoadingLabel('Identifying services...');
+
+		// Connect to the appropriate BLE service
+		device.readServices([app.DFRBLU_SERVICE_UUID], onServiceSuccess, onServiceFailure);
+	}
+
+	function onConnectFailure(errorCode)
+	{
+		// Disconnect and show an error message to the user.
+		app.disconnect('Failed to connect to device');
+
+		// Write debug information to console
+		console.log('Error ' + errorCode);
+	}
+
+	// Stop scanning
+	evothings.easyble.stopScan();
+
+	// Connect to our device
+	console.log('Identifying service for communication');
+	device.connect(onConnectSuccess, onConnectFailure);
 };
 
-app.initialize();
+app.sendData = function(data)
+{
+	if (app.connected)
+	{
+		function onMessageSendSucces()
+		{
+			console.log('Succeded to send message.');
+		}
 
+		function onMessageSendFailure(errorCode)
+		{
+			console.log('Failed to send data with error: ' + errorCode);
+			app.disconnect('Failed to send data');
+		}
 
-function ReceiveMessage(evt) {
-    document.getElementById("taRecvMessage").innerHTML = "messaggio ricevuto";
-    var message = evt.data.split('__separatore_postmessage__')[0];
-    var callbackJS = evt.data.split('__separatore_postmessage__')[1];
-    var origine = evt.origin;
-    var ta = document.getElementById("taRecvMessage");
-    if (ta == null)
-        alert(message);
-    else {
-        document.getElementById("taRecvMessage").innerHTML = message + ' da [' + origine + ']';
-        eval(evt.data);
-    }
+		data = new Uint8Array(data);
 
-    evt.source.postMessage(callbackJS, origine);
-}
+		app.device.writeCharacteristic(
+			app.DFRBLU_CHAR_RXTX_UUID,
+			data,
+			onMessageSendSucces,
+			onMessageSendFailure);
+	}
+	else
+	{
+		// Disconnect and show an error message to the user.
+		app.disconnect('Disconnected');
+
+		// Write debug information to console
+		console.log('Error - No device connected.');
+	}
+};
+
+app.receivedData = function(data)
+{
+	if (app.connected)
+	{
+		var data = new Uint8Array(data);
+
+		if (data[0] === 0xAD)
+		{
+			console.log('Data received: [' +
+				data[0] +', ' + data[1] +', ' + data[2] + ']');
+
+			var value = (data[2] << 8) | data[1];
+
+			console.log(value);
+
+			$('#analogDigitalResult').text(value);
+		}
+	}
+	else
+	{
+		// Disconnect and show an error message to the user.
+		app.disconnect('Disconnected');
+
+		// Write debug information to console
+		console.log('Error - No device connected.');
+	}
+};
+
+app.disconnect = function(errorMessage)
+{
+	if (errorMessage)
+	{
+		navigator.notification.alert(errorMessage, function() {});
+	}
+
+	app.connected = false;
+	app.device = null;
+
+	// Stop any ongoing scan and close devices.
+	evothings.easyble.stopScan();
+	evothings.easyble.closeConnectedDevices();
+
+	console.log('Disconnected');
+
+	$('#scanResultView').hide();
+	$('#scanResultView').empty();
+	$('#controlView').hide();
+	$('#startView').show();
+};
